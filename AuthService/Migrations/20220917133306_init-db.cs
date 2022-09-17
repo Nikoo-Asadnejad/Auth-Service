c4218ff6e3d003744174d96bb02906ce8d186de4
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-09-17 18:05:08 +0430

    Chores : add migration

---
+++ AuthService/Migrations/20220917133306_init-db.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Migrations
+{
+    public partial class initdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    CellPhone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Ip = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserAgent = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    LastAuthCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    EditDate = table.Column<long>(type: "bigint", nullable: true),
+                    EditedBy = table.Column<long>(type: "bigint", nullable: true),
+                    DeleteDate = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OptCodes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    IsSent = table.Column<bool>(type: "bit", nullable: true),
+                    ExpireDate = table.Column<long>(type: "bigint", nullable: true),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateDate = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    EditDate = table.Column<long>(type: "bigint", nullable: true),
+                    EditedBy = table.Column<long>(type: "bigint", nullable: true),
+                    DeleteDate = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OptCodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OptCodes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTokenModel",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsExpired = table.Column<bool>(type: "bit", nullable: false),
+                    ExpireDate = table.Column<long>(type: "bigint", nullable: true),
+                    CreateDate = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    EditDate = table.Column<long>(type: "bigint", nullable: true),
+                    EditedBy = table.Column<long>(type: "bigint", nullable: true),
+                    DeleteDate = table.Column<long>(type: "bigint", nullable: true),
+                    DeletedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokenModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserTokenModel_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OptCodes_UserId",
+                table: "OptCodes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTokenModel_UserId",
+                table: "UserTokenModel",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OptCodes");
+
+            migrationBuilder.DropTable(
+                name: "UserTokenModel");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
